2f4dc8c3b3df9cd1ab8eac5b8716b909b1246a34
Alec Coria <alec_coria@hotmail.com>
2021-06-12 17:23:42 -0500

    47

--- mapa.js
+++ mapa.js
@@ -284,7 +284,7 @@ function iniciaMapa() {
 
     var icono = {
         url: "./img/circle.png",
-        scaledSize: new google.maps.Size(35,35),
+        scaledSize: new google.maps.Size(30,30),
         origin: new google.maps.Point(0,0),
         anchor: new google.maps.Point(0,0)
     }
